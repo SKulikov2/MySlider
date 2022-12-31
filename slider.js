1b6965f2c0a89fdee8f17b0884bbfbd0092f1f0b
SKulikov2 <Kulikovsv.1987@mail.ru>
2022-12-31 09:44:01 +0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -7,6 +7,35 @@ const bottom = document.querySelector('.bottom')
 let slideNumber = 1
 const length = images.length
 
+for(let i = 0; i < length; i++) {
+    const div = document.createElement('div')
+    div.className = 'button'
+    bottom.appendChild(div)
+}
+
+//Красим первый элемент в белый цвет
+const buttons = document.querySelectorAll('.button')
+buttons[0].style.backgroundColor = 'white'
+
+//Функция для корректировки цвета кнопок
+const resetBg = () => {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = 'transparent'
+    })
+}
+
+
+//переключаем слайды нажатием на вновь созданные кнопки внизу и меняем цвет кнопок
+buttons.forEach((button, index) => {
+    button.addEventListener('click',()=>{
+        resetBg()
+slider.style.transform = `translateX(-${index * 600}px)`
+slideNumber = index + 1
+button.style.backgroundColor = 'white'
+    })
+})
+
+
 //листаем вправо
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 600}px)`
@@ -25,47 +54,28 @@ const getFirstSlide = () => {
     slideNumber = 1
 }
 
+// Функция для изменения цвета
+const changeColor = () => {
+    resetBg()
+    buttons[slideNumber-1].style.backgroundColor = 'white'
+}
+
 // к последнему слайду
 const getLastSlide = () => {
     slider.style.transform = `translateX(-${(length-1)*600}px)`
     slideNumber = length
 }
 
+
 right.addEventListener('click', ()=>{
-slideNumber < length ? nextSlide() : getFirstSlide()
+    slideNumber < length ? nextSlide() : getFirstSlide()
+    changeColor()
 })
 
 
 left.addEventListener('click', () => {
     slideNumber > 1 ? prevSlide() : getLastSlide()
+    changeColor()
 })
 
 
-for(let i = 0; i < length; i++) {
-    const div = document.createElement('div')
-    div.className = 'button'
-    bottom.appendChild(div)
-}
-
-//Объявил buttons здесь, так как мы создали их чуть выше. 
-//Если бы объявил buttons  в начале, то так бы не сработало
-//Красим первый элемент в белый цвет
-const buttons = document.querySelectorAll('.button')
-buttons[0].style.backgroundColor = 'white'
-
-
-const resetBg = () => {
-    buttons.forEach((button) => {
-        button.style.backgroundColor = 'transparent'
-    })
-}
-
-
-//переключаем слайды нажатием на вновь созданные кнопки внизу и меняем цвет кнопок
-buttons.forEach((button, index) => {
-    button.addEventListener('click',()=>{
-        resetBg()
-slider.style.transform = `translateX(-${index * 600}px)`
-button.style.backgroundColor = 'white'
-    })
-})
